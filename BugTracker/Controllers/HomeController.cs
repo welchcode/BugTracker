b26fe3da45973f055f978a166ffbfa934b29b3b5
alec <welchalec@gmail.com>
2020-04-01 21:53:41 -0700

    passing data to Update View

--- BugTracker/Controllers/HomeController.cs
+++ BugTracker/Controllers/HomeController.cs
@@ -15,8 +15,109 @@ namespace BugTracker.Controllers
             return View();
         }
 
-        public IActionResult UpdateBug()
+        public IActionResult UpdateBug(int BugIdParameter)
         {
+            string BugId, BugDescription, CreatedBy, UpdatedBy, Comment, Author;
+            DateTime BugCommentDate;
+
+
+            string SqlString = "SELECT * FROM BugTrackerDatabase.dbo.BugTable " +
+                               "WHERE BugId = '" + BugIdParameter + "';";
+
+            Utilities.Command = new System.Data.SqlClient.SqlCommand(SqlString, Utilities.Connection);
+
+            Utilities.Connection.Open();
+
+            using (Utilities.Reader = Utilities.Command.ExecuteReader())
+            {
+                Utilities.Reader.Read();
+                if (!Utilities.Reader.IsDBNull(0))
+                {
+                    BugId = Utilities.Reader.GetInt32(0).ToString();
+                }
+                else
+                {
+                    BugId = null;
+                }
+
+                if (!Utilities.Reader.IsDBNull(1))
+                {
+                    BugDescription = Utilities.Reader.GetString(1);
+                }
+                else
+                {
+                    BugDescription = null;
+                }
+
+                if (!Utilities.Reader.IsDBNull(2))
+                {
+                    CreatedBy = Utilities.Reader.GetString(2);
+                }
+                else
+                {
+                    CreatedBy = null;
+                }
+
+                if (!Utilities.Reader.IsDBNull(3))
+                {
+                    UpdatedBy = Utilities.Reader.GetString(3);
+                }
+                else
+                {
+                    UpdatedBy = null;
+                }
+            }
+
+            BugModel Bug = new BugModel { BugId = BugId, BugDescription = BugDescription, CreatedBy = CreatedBy, UpdatedBy = UpdatedBy };
+            ViewBag.Bug = Bug;
+
+
+            SqlString = "SELECT DateTimeCreated, BugComment, Author FROM BugTrackerDatabase.dbo.BugComments " +
+                        "WHERE BugId = " + Bug.BugId;
+
+            List<BugCommentModel> ListOfBugComments = new List<BugCommentModel>();
+
+            Utilities.Command = new System.Data.SqlClient.SqlCommand(SqlString, Utilities.Connection);
+
+            using (Utilities.Reader = Utilities.Command.ExecuteReader())
+            {
+                while (Utilities.Reader.Read())
+                {
+                    if (!Utilities.Reader.IsDBNull(0))
+                    {
+                        BugCommentDate = Utilities.Reader.GetDateTime(0);
+                    }
+                    else
+                    {
+                        BugCommentDate = default;
+                    }
+
+                    if (!Utilities.Reader.IsDBNull(1))
+                    {
+                        Comment = Utilities.Reader.GetString(1);
+                    }
+                    else
+                    {
+                        Comment = null;
+                    }
+
+                    if (!Utilities.Reader.IsDBNull(2))
+                    {
+                        Author = Utilities.Reader.GetString(2);
+                    }
+                    else
+                    {
+                        Author = null;
+                    }
+
+                    ListOfBugComments.Add(new BugCommentModel { BugId = Bug.BugId, Author = Author, BugComment = Comment, BugCommentDate = BugCommentDate });
+                }
+            }
+
+            ViewBag.ListOfBugComments = ListOfBugComments;
+
+            Utilities.Connection.Close();
+
             return View();
         }
 
@@ -101,6 +202,7 @@ namespace BugTracker.Controllers
                     BugsList.Add(new BugModel { BugId = BugId, BugDescription = BugDescription, CreatedBy = CreatedBy, UpdatedBy = UpdatedBy });
                 }
             }
+            Utilities.Connection.Close();
             return View(BugsList);
         }
     }
---
+++ BugTracker/Models/BugCommentModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTracker.Models
+{
+    public class BugCommentModel
+    {
+        public string BugId { get; set; }
+        public string BugComment { get; set; }
+        public string Author { get; set; }
+        public DateTime BugCommentDate { get; set; }
+    }
+}
